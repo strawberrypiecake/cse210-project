3dc6763185b7015ef5cb031ee08c872b881eb2dd
strawberrypiecake <jon24041@byui.edu>
2025-07-21 19:40:47 -0600

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        bool continuePrompt = true;
+
+        while(continuePrompt)
+        {
+            Console.Write("What is your guess? ");
+            string inputGuess = Console.ReadLine();
+            int magicGuess = int.Parse(inputGuess);
+
+            if(magicGuess > magicNumber)
+            {
+
+                Console.WriteLine("Lower");
+                Console.WriteLine();
+
+            }
+            else if(magicGuess < magicNumber)
+            {
+
+                Console.WriteLine("Higher");
+                Console.WriteLine();
+
+            }
+            else
+            {
+
+                Console.WriteLine("You guessed it!");
+                continuePrompt = false;
+
+            }
+
+        }
+
     }
 }

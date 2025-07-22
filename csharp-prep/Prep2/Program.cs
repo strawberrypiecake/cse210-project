0606e83d059f887abe627a33ae4a5ec0f490befc
strawberrypiecake <jon24041@byui.edu>
2025-07-21 19:40:19 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Prompts student to enter grade percentage and parses value into an integer variable.
+        Console.WriteLine();
+        Console.WriteLine("WHAT IS MY GRADE?");
+        Console.WriteLine("_________________");
+        Console.WriteLine();
+        Console.Write("Please, provide your grade as a percentage: ");
+        string gradeInput = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeInput);
+        string letterGrade;
+
+        //Conditional statements that determine letter grade.
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your final grade is: {letterGrade}");
+        Console.WriteLine();
+
+        //Additional conditional statements that print message according to their ability to pass the class.
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations. You have successfully passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You have not been able to pass the class at this time. You got this, keep your head up and let's get it this time!");
+        }
     }
 }

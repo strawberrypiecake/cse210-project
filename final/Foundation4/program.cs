5e1d1bee0b79dcafdd3aaae59209dd45dee0becb
strawberrypiecake <jon24041@byui.edu>
2026-04-04 23:27:09 -0600

    Change activity dates in program.cs
    Updated activity dates from 2022 to 2026.

--- final/Foundation4/program.cs
+++ final/Foundation4/program.cs
@@ -7,9 +7,9 @@ class Program
     {
         List<Activity> activities = new List<Activity>
         {
-            new Running(new DateTime(2022, 11, 3), 30, 3.0),
-            new Cycling(new DateTime(2022, 11, 3), 45, 15.0),
-            new Swimming(new DateTime(2022, 11, 3), 30, 20)
+            new Running(new DateTime(2026, 11, 3), 30, 3.0),
+            new Cycling(new DateTime(2026, 11, 3), 45, 15.0),
+            new Swimming(new DateTime(2026, 11, 3), 30, 20)
         };
 
         foreach (var activity in activities)

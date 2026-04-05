b7332bfea12e3789306b3344c88338ddad1f2477
strawberrypiecake <jon24041@byui.edu>
2026-04-04 23:11:05 -0600

    Refactor Product class structure and access modifiers
    Refactored Product class to use public fields and simplified methods.

--- final/Foundation2/product.cs
+++ final/Foundation2/product.cs
@@ -1,24 +1,20 @@
 public class Product
 {
-    private string _name;
-    private string _productId;
-    private decimal _pricePerUnit;
-    private int _quantity;
+    public string name;
+    public string productId;
+    public decimal pricePerUnit;
+    public int quantity;
 
     public Product(string name, string productId, decimal pricePerUnit, int quantity)
     {
-        _name = name;
-        _productId = productId;
-        _pricePerUnit = pricePerUnit;
-        _quantity = quantity;
+        this.name = name;
+        this.productId = productId;
+        this.pricePerUnit = pricePerUnit;
+        this.quantity = quantity;
     }
 
-    public string GetName() => _name;
-    public string GetProductId() => _productId;
-    public decimal GetTotalCost() => _pricePerUnit * _quantity;
-
-    public void SetName(string name) => _name = name;
-    public void SetProductId(string id) => _productId = id;
-    public void SetPricePerUnit(decimal price) => _pricePerUnit = price;
-    public void SetQuantity(int qty) => _quantity = qty;
+    public decimal GetTotalCost()
+    {
+        return pricePerUnit * quantity;
+    }
 }
